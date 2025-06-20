68864fa52caef17a1f8995e89244b3bcd10cf753
tarunmac29 <tarunmachhi29@gmail.com>
2025-06-20 10:28:30 +0530

    dependency installed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 0.2s ease-out", // 👈 custom animation
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0, transform: "scale(0.95)" },
+          "100%": { opacity: 1, transform: "scale(1)" },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
